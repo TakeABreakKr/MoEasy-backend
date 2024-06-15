602804d4a57be80578c81cef157c33bf89085f4e
seongyong.choi <seongyong.choi1@cj.net>
2024-06-15 21:09:58 +0900

    use type in bootstrap

--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,13 @@ import { NestFactory } from '@nestjs/core';
 import { ConfigService } from '@nestjs/config';
 import { AppModule } from './app.module';
 import { setupSwagger } from './config/swagger.config';
-import { envEnum } from './enums/env.enum';
+import { envEnum, envEnumType } from './enums/env.enum';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const configService = app.get(ConfigService);
+  const configService: ConfigService = app.get(ConfigService);
 
-  const env = configService.get('env');
+  const env: envEnumType = configService.get('env');
   if (env !== envEnum.PROD) {
     setupSwagger(app);
   }
