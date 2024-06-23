26db869f406810d968f743590b8280f379e2b12e
seongyong.choi <seongyong.choi1@cj.net>
2024-06-23 23:14:37 +0900

    fix prettier

--- src/domain/user/dao/users.dao.ts
+++ src/domain/user/dao/users.dao.ts
@@ -6,9 +6,7 @@ import { Users } from '../entity/users.entity';
 
 @Injectable()
 export class UsersDao {
-  constructor(
-    @InjectRepository(Users) private usersRepository: Repository<Users>,
-  ) {}
+  constructor(@InjectRepository(Users) private usersRepository: Repository<Users>) {}
 
   async findById(id: number): Promise<Users | null> {
     return this.usersRepository.findOneBy({ users_id: id });
