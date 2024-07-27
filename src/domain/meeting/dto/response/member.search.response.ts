23e93cd0b56d0281b32580a35a4e4fc73adc7459
seongyong.choi <seongyong.choi1@cj.net>
2024-07-27 11:19:34 +0900

    swagger nested response type

--- src/domain/meeting/dto/response/member.search.response.ts
+++ src/domain/meeting/dto/response/member.search.response.ts
@@ -2,6 +2,9 @@ import { ApiProperty } from '@nestjs/swagger';
 import { MemberSearchDto } from './member.search.dto';
 
 export class MemberSearchResponse {
-  @ApiProperty()
+  @ApiProperty({
+    type: MemberSearchDto,
+    isArray: true,
+  })
   memberList: MemberSearchDto[];
 }
