be84eb251182736d067067f813a039e667a387da
seongyong.choi <seongyong.choi1@cj.net>
2024-07-27 11:26:14 +0900

    path alias

--- src/domain/meeting/dto/response/meeting.member.dto.ts
+++ src/domain/meeting/dto/response/meeting.member.dto.ts
@@ -1,10 +1,12 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { AuthorityEnumType } from '@enums/authority.enum';
+import { AuthorityEnum, AuthorityEnumType } from '@enums/authority.enum';
 
 export class MeetingMemberDto {
   @ApiProperty()
   username: string;
 
-  @ApiProperty()
+  @ApiProperty({
+    enum: AuthorityEnum,
+  })
   authority: AuthorityEnumType;
 }
