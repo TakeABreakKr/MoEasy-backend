19d14c2f8446a20d37da733d27d33bf54c22bc85
최성용 <seongyong.choi1@cj.net>
2024-09-07 11:27:04 +0900

    Feature/token parse (#13)
    * token 고도화
    
    * requester_id from token
    
    * token이 계속 바뀌도록 발행 시간 payload에 추가

--- src/domain/user/controller/auth.controller.ts
+++ src/domain/user/controller/auth.controller.ts
@@ -4,19 +4,28 @@ import type { AuthCallbackRequest } from '../dto/request/auth.callback.request';
 import { Body, Controller, Post, Res } from '@nestjs/common';
 import { AuthService } from '../service/auth.service';
 import { ApiTags } from '@nestjs/swagger';
+import { Public } from '@decorator/public.decorator';
 
 @ApiTags('auth')
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
 
+  @Public()
   @Post('login')
   async login() {
     await this.authService.login();
   }
 
+  @Public()
   @Post('callback')
   async callback(@Body() req: AuthCallbackRequest, @Res() res: Response) {
     await this.authService.callback(req, res);
+  }
+
+  @Public()
+  @Post('refresh')
+  refreshAccessToken(@Body() refreshToken: string): string {
+    return this.authService.refreshAccessToken(refreshToken);
   }
 }
