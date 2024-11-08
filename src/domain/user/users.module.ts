f0e87103d38483c06e58f9ed477b1071b4ff3eb8
seongyong.choi <seongyong.choi1@cj.net>
2024-11-08 23:48:58 +0900

    제거되지 못한 module dependency 정리

--- src/domain/user/users.module.ts
+++ src/domain/user/users.module.ts
@@ -4,13 +4,12 @@ import { Users } from './entity/users.entity';
 import { UsersDao } from './dao/users.dao';
 import { AuthService } from './service/auth.service';
 import { AuthController } from './controller/auth.controller';
-import { UserComponent } from '../discord/component/user.component';
 import { DiscordModule } from '@domain/discord/discord.module';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Users]), DiscordModule],
   controllers: [AuthController],
-  providers: [UsersDao, UserComponent, AuthService],
+  providers: [UsersDao, AuthService],
   exports: [UsersDao],
 })
 export class UsersModule {}
