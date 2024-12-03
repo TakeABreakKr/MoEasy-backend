47db8752ac9ad9afc3278bccbc0070b596fc7e36
hangyeol17 <81200079+hangyeol17@users.noreply.github.com>
2024-12-03 20:24:58 +0900

    notification component test

---
+++ src/domain/notification/component/notification.component.spec.ts
@@ -0,0 +1,47 @@
+import { NotificationDao } from '@domain/notification/dao/notification.dao.interface';
+import { NotificationComponent } from '@domain/notification/component/notification.component';
+import { Test, TestingModule } from '@nestjs/testing';
+import { Notification } from '@domain/notification/entity/notification.entity';
+
+class MockNotificationDao implements NotificationDao {
+  async getListByUserId(userId: number): Promise<Notification[]> {
+    return [];
+  }
+
+  async save(): Promise<void> {}
+  async saveAll(): Promise<void> {}
+
+  async getByIdList(notificationIdList: number[]): Promise<Notification[]> {
+    return [];
+  }
+}
+
+describe('NotificationComponent', () => {
+  let notificationComponent: NotificationComponent;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [
+        {
+          provide: 'NotificationDao',
+          useClass: MockNotificationDao,
+        },
+      ],
+    }).compile();
+    notificationComponent = module.get<NotificationComponent>('NotificationComponent');
+  });
+
+  it('addNotification', async () => {
+    const content: string = 'test';
+    const userId: number = 1;
+
+    expect(notificationComponent.addNotification(content, userId)).toBe(void 0);
+  });
+
+  it('addNotifications', async () => {
+    const content: string = "it's test!";
+    const userIdList: number[] = [1];
+
+    expect(notificationComponent.addNotifications(content, userIdList)).toBe(void 0);
+  });
+});
