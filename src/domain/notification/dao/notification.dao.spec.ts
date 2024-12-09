75d089205077bf9070b3193d780add5ae926fa20
hangyeol17 <81200079+hangyeol17@users.noreply.github.com>
2024-12-09 16:17:40 +0900

    DAO

--- src/domain/notification/dao/notification.dao.spec.ts
+++ src/domain/notification/dao/notification.dao.spec.ts
@@ -2,28 +2,53 @@ import { NotificationDao } from '@domain/notification/dao/notification.dao.inter
 import { Test, TestingModule } from '@nestjs/testing';
 import { NotificationDaoImpl } from '@domain/notification/dao/notification.dao';
 import { Repository } from 'typeorm';
+import { Notification } from '@domain/notification/entity/notification.entity';
+import { FindOptionsWhere } from 'typeorm/find-options/FindOptionsWhere';
 import { getRepositoryToken } from '@nestjs/typeorm';
 
-describe('NotificationDao', async () => {
-  let notificationDao: NotificationDao;
-  let notificationRepository: Repository<Notification>;
+class MockNotificationRepository extends Repository<Notification> {
+  option: FindOptionsWhere<Notification> = {
+    users_id: undefined,
+  };
 
-  const notificationRepositoryToken: string | Function = getRepositoryToken(Notification);
+  async save(): Promise<any[]> {
+    return;
+  }
+
+  async findBy(where: FindOptionsWhere<Notification> | FindOptionsWhere<Notification>[]): Promise<Notification[]> {
+    const notification = Notification.create('아무말', 1);
+    const notification2 = Notification.create('알림', 2);
+    const condition: FindOptionsWhere<Notification> = { users_id: 1 };
+    const condition2: FindOptionsWhere<Notification>[] = [condition, { users_id: 2 }];
+    if (where.users_id == condition.users_id) return [notification];
+    if (where == condition2) return [notification, notification2];
+    console.log(where);
+    return [];
+  }
+}
+
+describe('NotificationDao', () => {
+  let notificationDao: NotificationDao;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [],
       providers: [
-        { provide: 'notificationDao', useClass: NotificationDaoImpl },
-        { provide: notificationRepositoryToken, useClass: Repository },
+        { provide: 'NotificationDao', useClass: NotificationDaoImpl },
+        { provide: getRepositoryToken(Notification), useClass: MockNotificationRepository },
       ],
     }).compile();
     notificationDao = module.get<NotificationDao>('NotificationDao');
-    notificationRepository = module.get<Repository<Notification>>(notificationRepositoryToken);
   });
 
-  it('getListByUserId', async () => {});
+  it('getListByUserId', async () => {
+    const userId: number = 1;
+    const result = await notificationDao.getListByUserId(userId);
+    const notification = Notification.create('아무말', 1);
+    expect(result).toStrictEqual(notification);
+  });
 
-  it('getListByUserId - success case : not found', async () => {});
+  it('getListByUserId - case : not found', async () => {});
 
   it('save', async () => {});
 
