1c290d41d3e023031da39c829a3955365e5ef741
doyeon kim <kim2004on@gmail.com>
2024-12-12 18:32:22 +0900

    meeting AuthorityComponent 개선 - @Inject를 사용하여 MemberDao를 문자열 토큰으로 주입 - validateAuthority 메서드에서 멤버 존재 여부와 권한 검증 로직을 분리

--- src/domain/meeting/component/authority.component.ts
+++ src/domain/meeting/component/authority.component.ts
@@ -1,13 +1,13 @@
 import { AuthorityEnumType, MANAGING_AUTHORITIES } from '@enums/authority.enum';
 import { Member } from '@domain/meeting/entity/member.entity';
-import { BadRequestException, Injectable } from '@nestjs/common';
+import { BadRequestException, Inject, Injectable } from '@nestjs/common';
 import { ErrorMessageType } from '@enums/error.message.enum';
 import { MemberDao } from '@domain/meeting/dao/member.dao.interface';
 import { AuthorityComponent } from './authority.component.interface';
 
 @Injectable()
 export class AuthorityComponentImpl implements AuthorityComponent {
-  constructor(private memberDao: MemberDao) {}
+  constructor(@Inject('MemberDao') private memberDao: MemberDao) {}
 
   public async validateAuthority(
     requester_id: number,
@@ -16,7 +16,11 @@ export class AuthorityComponentImpl implements AuthorityComponent {
   ): Promise<void> {
     const member: Member | null = await this.memberDao.findByUsersAndMeetingId(requester_id, meetingId);
 
-    if (!member || !validAuthorities.includes(member.authority)) {
+    if (!member) {
+      throw new BadRequestException(ErrorMessageType.NOT_FOUND_MEMBER);
+    }
+
+    if (!validAuthorities.includes(member.authority)) {
       throw new BadRequestException(ErrorMessageType.UNAUTHORIZED_ACCESS);
     }
   }
