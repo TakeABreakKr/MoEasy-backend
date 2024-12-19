7af60ce28ba797ab19e8828d0b0448a9656e935d
doyeon kim <kim2004on@gmail.com>
2024-12-20 00:40:31 +0900

    meeting - authorityComponent 수정 - new, setter 제거 - dao create, findByUsersAndMeetingId 모킹 개선 - 생성되고 조회한 거 바탕으로 테스트 구성

--- src/domain/meeting/component/authority.component.spec.ts
+++ src/domain/meeting/component/authority.component.spec.ts
@@ -1,29 +1,42 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { MemberDao } from '../dao/member.dao.interface';
-import { AuthorityEnum, AuthorityEnumType, MANAGING_AUTHORITIES } from '@root/enums/authority.enum';
+import { AuthorityEnum } from '@root/enums/authority.enum';
 import { Member } from '../entity/member.entity';
 import { AuthorityComponent } from './authority.component.interface';
 import { AuthorityComponentImpl } from './authority.component';
 import { BadRequestException } from '@nestjs/common';
 import { ErrorMessageType } from '@root/enums/error.message.enum';
+import { CreateMemberDto } from '../dto/create.member.dto';
 
 class MockMemberDao implements MemberDao {
-  private mockMember: Member | null = null;
+  private mockMember: Member[] = [];
 
-  setMockMember(member: Member | null) {
-    this.mockMember = member;
+  async create({ authority = AuthorityEnum.WAITING, ...props }: CreateMemberDto): Promise<Member> {
+    const member = {
+      users_id: props.usersId,
+      meeting_id: props.meetingId,
+      authority,
+      applicationMessage: null,
+      createdAt: null,
+      updatedAt: null,
+      user: null,
+      meeting: null,
+      getUser: null,
+      getMeeting: null,
+      updateAuthority() {},
+    };
+
+    this.mockMember.push(member);
+    return member;
   }
 
-  async findByUsersAndMeetingId(): Promise<Member | null> {
-    return this.mockMember;
+  async findByUsersAndMeetingId(usersId: number, meetingId: number): Promise<Member | null> {
+    return this.mockMember.find((member) => member.users_id === usersId && member.meeting_id === meetingId) || null;
   }
 
   async saveAll(): Promise<void> {}
   async updateAuthority(): Promise<void> {}
   async deleteByUsersAndMeetingId(): Promise<void> {}
-  async create(): Promise<Member> {
-    return new Member();
-  }
   async findByUserId(): Promise<Member[]> {
     return [];
   }
@@ -37,15 +50,14 @@ class MockMemberDao implements MemberDao {
 
 describe('AuthorityComponent', () => {
   let authorityComponent: AuthorityComponent;
-  let mockMemberDao: MockMemberDao;
+  let memberDao: MemberDao;
 
   beforeEach(async () => {
-    mockMemberDao = new MockMemberDao();
     const module: TestingModule = await Test.createTestingModule({
       providers: [
         {
           provide: 'MemberDao',
-          useValue: mockMemberDao,
+          useClass: MockMemberDao,
         },
         {
           provide: 'AuthorityComponent',
@@ -54,44 +66,33 @@ describe('AuthorityComponent', () => {
       ],
     }).compile();
     authorityComponent = module.get<AuthorityComponent>('AuthorityComponent');
+    memberDao = module.get<MemberDao>('MemberDao');
   });
 
   it('validateAuthorityTest - SUCCESS ', async () => {
-    const requester_id = 100;
-    const meetingId = 100;
-    const validAuthorities: AuthorityEnumType[] = MANAGING_AUTHORITIES;
+    await memberDao.create({
+      meetingId: 1,
+      usersId: 1,
+      authority: AuthorityEnum.OWNER,
+    });
 
-    const mockMember = new Member();
-    mockMember.authority = AuthorityEnum.MANAGER;
-    mockMemberDao.setMockMember(mockMember);
-
-    await expect(
-      authorityComponent.validateAuthority(requester_id, meetingId, validAuthorities),
-    ).resolves.not.toThrow();
+    await expect(authorityComponent.validateAuthority(1, 1, [AuthorityEnum.OWNER])).resolves.not.toThrow();
   });
 
   it('validateAuthorityTest - NOT_FOUND_MEMBER ', async () => {
-    const requester_id = 200;
-    const meetingId = 200;
-    const validAuthorities: AuthorityEnumType[] = MANAGING_AUTHORITIES;
-
-    mockMemberDao.setMockMember(null);
-
-    await expect(authorityComponent.validateAuthority(requester_id, meetingId, validAuthorities)).rejects.toThrow(
+    await expect(authorityComponent.validateAuthority(100, 100)).rejects.toThrow(
       new BadRequestException(ErrorMessageType.NOT_FOUND_MEMBER),
     );
   });
 
   it('validateAuthorityTest - UNAUTHORIZED_ACCESS ', async () => {
-    const requester_id = 300;
-    const meetingId = 300;
-    const validAuthorities: AuthorityEnumType[] = [AuthorityEnum.MANAGER];
+    await memberDao.create({
+      meetingId: 3,
+      usersId: 3,
+      authority: AuthorityEnum.MEMBER,
+    });
 
-    const mockMember = new Member();
-    mockMember.authority = AuthorityEnum.MEMBER;
-    mockMemberDao.setMockMember(mockMember);
-
-    await expect(authorityComponent.validateAuthority(requester_id, meetingId, validAuthorities)).rejects.toThrow(
+    await expect(authorityComponent.validateAuthority(3, 3)).rejects.toThrow(
       new BadRequestException(ErrorMessageType.UNAUTHORIZED_ACCESS),
     );
   });
