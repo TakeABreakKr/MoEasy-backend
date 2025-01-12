524022434bd1578dfdabe016a9e0b9a7bfd7ad30
doyeon kim <kim2004on@gmail.com>
2025-01-12 21:18:25 +0900

    utils - sort에서 Sortable 인터페이스를 재사용을 위해 분리

--- src/utils/schedule.utils.ts
+++ src/utils/schedule.utils.ts
@@ -4,6 +4,7 @@ export class ScheduleUtils {
   public static reminderListToMask(reminderList: ReminderEnumType[]): number {
     let mask: number = 0;
     const enumList = Object.keys(ReminderEnum);
+
     reminderList.forEach((reminder) => {
       const index: number = enumList.indexOf(reminder);
       mask += 2 ** index; // 2^index
@@ -15,6 +16,7 @@ export class ScheduleUtils {
   public static maskToReminderList(mask: number): ReminderEnumType[] {
     const binary: string = mask.toString(2);
     const enumList = Object.keys(ReminderEnum);
+
     return enumList
       .filter((reminderKey) => binary.charAt(enumList.indexOf(reminderKey)) === '1')
       .map((reminderKey: ReminderEnumKeyType): ReminderEnumType => ReminderEnum[reminderKey]);
