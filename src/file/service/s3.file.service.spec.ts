b7b523f9cd3ee1d7a1bfe26846dee5a0d52884a1
김도연 <kim2004on@gmail.com>
2025-01-22 17:46:47 +0900

    file unit test (#33)
    * file - local, s3 서비스 유닛 테스트 생성
    
    * file - s3 서비스에서 변수 이름 변경
    
    * file - local, s3 서비스 유닛 테스트에서 변수 이름 변경
    
    * file - s3 파일 서비스 및 유닛 테스트 변수 이름 수정

---
+++ src/file/service/s3.file.service.spec.ts
@@ -0,0 +1,80 @@
+import { Test } from '@nestjs/testing';
+import { ConfigService } from '@nestjs/config';
+import { S3FileService } from './s3.file.service';
+import { StreamableFile } from '@nestjs/common';
+
+class MockConfigService extends ConfigService {
+  get(key: string) {
+    const config = {
+      'AWS.region': 'ap-northeast-2',
+      'AWS.S3_ACCESS_KEY': 'aws-s3-access-key',
+      'AWS.S3_SECRET_ACCESS_KEY': 'aws-s3-secret-access-secret',
+      'AWS.S3_BUCKET_NAME': 'aws-s3-bucketname',
+      'AWS.bucket': 'urlBucketName',
+    };
+
+    return config[key];
+  }
+}
+
+jest.mock('@aws-sdk/client-s3', () => ({
+  S3Client: jest.fn().mockImplementation(() => ({
+    send: jest.fn().mockImplementation(() => ({
+      Body: {
+        transformToByteArray: async () => new Uint8Array(Buffer.from('test')),
+      },
+    })),
+  })),
+
+  PutObjectCommand: jest.fn(),
+  GetObjectCommand: jest.fn(),
+}));
+
+describe('S3FileService', () => {
+  let s3FileService: S3FileService;
+
+  beforeEach(async () => {
+    const module = await Test.createTestingModule({
+      providers: [
+        S3FileService,
+        {
+          provide: ConfigService,
+          useClass: MockConfigService,
+        },
+      ],
+    }).compile();
+
+    s3FileService = module.get<S3FileService>(S3FileService);
+  });
+
+  it('uploadThumbnailFileTest', async () => {
+    const file = {
+      fieldname: 'thumbnail',
+      originalname: 'MOEASY.jpg',
+      encoding: '7bit',
+      mimetype: 'image/jpeg',
+      size: 1234,
+      buffer: Buffer.from('test'),
+      stream: undefined,
+      destination: '',
+      filename: 'testFilenameMoeasy.jpg',
+      path: '',
+    };
+    const path = await s3FileService.uploadThumbnailFile(file);
+
+    expect(path).toBe('https://s3.ap-northeast-2.amazonaws.com/urlBucketName/testFilenameMoeasy.jpg');
+  });
+
+  it('getFileTest', async () => {
+    const path = 'MOEASY.jpg';
+    const result = await s3FileService.getFile(path);
+
+    expect(result).toBeInstanceOf(StreamableFile);
+  });
+
+  it('getFileTest - EMPTY', async () => {
+    const result = await s3FileService.getFile('');
+
+    expect(result).toBeNull();
+  });
+});
