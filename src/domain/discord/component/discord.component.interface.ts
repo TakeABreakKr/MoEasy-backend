d2970ea76edfda3f798c38143ed613a3e7f604ae
doyeon kim <kim2004on@gmail.com>
2025-02-12 22:46:28 +0900

    discord : (discord, message) component interface 생성

---
+++ src/domain/discord/component/discord.component.interface.ts
@@ -0,0 +1,7 @@
+import { TokenDto } from '@root/service/auth/dto/token.dto';
+import { DiscordUserByTokenDto } from '../dto/response/discord.authorized.info.response';
+
+export interface DiscordComponent {
+  getTokens(code: string): Promise<TokenDto>;
+  getUser(tokens: TokenDto): Promise<DiscordUserByTokenDto>;
+}
