bba85c2ffd18cba79305fe7a60486494d037b423
doyeon kim <kim2004on@gmail.com>
2025-02-19 17:48:48 +0900

    activity - (participant, activity) component 유닛 테스트 생성

--- src/domain/activity/component/activity.component.spec.ts
+++ src/domain/activity/component/activity.component.spec.ts
@@ -1,97 +1,64 @@
 import { Test, TestingModule } from '@nestjs/testing';
-
 import { ActivityComponent } from '@domain/activity/component/activity.component.interface';
 import { ActivityCreateVO } from '@domain/activity/vo/activity.create.vo';
 import { ActivityDao } from '@domain/activity/dao/activity.dao.interface';
 import { Activity } from '@domain/activity/entity/activity.entity';
 import { ActivityComponentImpl } from '@domain/activity/component/activity.component';
 
-const daoAccessLog: string[] = [];
 class MockActivityDao implements ActivityDao {
-  static createLog: string = 'ActivityDao.create called';
-  static findByActivityIdLog: string = 'ActivityDao.findByActivityId called';
-  static updateLog: string = 'ActivityDao.update called';
-  static findAllByActivityIdsLog: string = 'ActivityDao.findAllByActivityIds called';
-  static findByMeetingIdLog: string = 'ActivityDao.findByMeetingId called';
-  static deleteLog: string = 'ActivityDao.delete called';
-
-  static activityCreateVOs: ActivityCreateVO[] = [
-    {
-      meetingId: '123',
-      name: 'Jane',
-      explanation: '모임2',
+  public mockActivitys: Activity[] = [
+    Activity.createForTest(100, {
+      meetingId: '64',
+      name: 'moeasy1',
+      explanation: '모임설명1',
       startDate: new Date(),
       endDate: new Date(),
       reminder: [],
-      announcement: '',
+      announcement: '공지사항1',
       onlineYn: true,
       address: null,
-      detailAddress: '강남',
-    },
-    {
-      meetingId: '234',
-      name: '정기모임1',
-      explanation: '강남',
+      detailAddress: '평택',
+    }),
+    Activity.createForTest(200, {
+      meetingId: 'C8',
+      name: 'moeasy2',
+      explanation: '모임설명2',
       startDate: new Date(),
       endDate: new Date(),
       reminder: [],
-      announcement: '',
+      announcement: '공지사항2',
       onlineYn: false,
       address: null,
-      detailAddress: '강남',
-    },
-    {
-      meetingId: '234',
-      name: '정기모임2',
-      explanation: '또 강남',
-      startDate: new Date(),
-      endDate: new Date(),
-      reminder: [],
-      announcement: '',
-      onlineYn: false,
-      address: null,
-      detailAddress: '강남',
-    },
-  ];
-
-  public static activitys: Activity[] = [
-    Activity.createForTest(100, MockActivityDao.activityCreateVOs[0]),
-    Activity.createForTest(200, MockActivityDao.activityCreateVOs[1]),
-    Activity.createForTest(300, MockActivityDao.activityCreateVOs[2]),
+      detailAddress: '수원',
+    }),
   ];
 
   async findByActivityId(activity_id: number): Promise<Activity | null> {
-    daoAccessLog.push(MockActivityDao.findByActivityIdLog);
-    return MockActivityDao.activitys.find((activity: Activity) => activity.activity_id === activity_id);
+    return this.mockActivitys.find((activity: Activity) => activity.activity_id === activity_id) || null;
   }
 
   async findAllByActivityIds(activity_ids: number[]): Promise<Activity[]> {
-    daoAccessLog.push(MockActivityDao.findAllByActivityIdsLog);
-    return MockActivityDao.activitys.filter((activity: Activity) => activity_ids.includes(activity.activity_id));
+    return this.mockActivitys.filter((activity: Activity) => activity_ids.includes(activity.activity_id));
   }
 
   async create(activityCreateVO: ActivityCreateVO): Promise<Activity> {
-    daoAccessLog.push(MockActivityDao.createLog);
     const activity = Activity.createForTest(101, activityCreateVO);
-    MockActivityDao.activitys.push(activity);
+    this.mockActivitys.push(activity);
+
     return activity;
   }
 
   async update(activity: Activity): Promise<void> {
-    daoAccessLog.push(MockActivityDao.updateLog);
-    MockActivityDao.activitys.push(activity);
+    const index = this.mockActivitys.findIndex((activity) => activity.activity_id === activity.activity_id);
+    this.mockActivitys[index] = activity;
   }
 
   async findByMeetingId(meeting_id: number): Promise<Activity[]> {
-    daoAccessLog.push(MockActivityDao.findByMeetingIdLog);
-    return MockActivityDao.activitys.filter((activity: Activity) => activity.meeting_id === meeting_id);
+    return this.mockActivitys.filter((activity: Activity) => activity.meeting_id === meeting_id);
   }
 
   async delete(activity_id: number): Promise<void> {
-    daoAccessLog.push(MockActivityDao.deleteLog);
-    MockActivityDao.activitys = MockActivityDao.activitys.filter(
-      (activity: Activity) => activity.activity_id !== activity_id,
-    );
+    this.mockActivitys = this.mockActivitys.filter((activity: Activity) => activity.activity_id !== activity_id);
   }
 }
 
@@ -99,69 +66,132 @@ describe('ActivityComponent', () => {
   let activityComponent: ActivityComponent;
 
   beforeEach(async () => {
-    daoAccessLog.length = 0;
-
     const module: TestingModule = await Test.createTestingModule({
       providers: [
-        { provide: 'ActivityDao', useClass: MockActivityDao },
-        { provide: 'ActivityComponent', useClass: ActivityComponentImpl },
+        {
+          provide: 'ActivityDao',
+          useClass: MockActivityDao,
+        },
+        {
+          provide: 'ActivityComponent',
+          useClass: ActivityComponentImpl,
+        },
       ],
     }).compile();
 
     activityComponent = module.get<ActivityComponent>('ActivityComponent');
   });
 
-  const activity1: ActivityCreateVO = {
-    meetingId: '123',
-    name: 'John',
-    explanation: '모임',
-    startDate: new Date(),
-    endDate: new Date(),
-    reminder: [],
-    announcement: '',
-    onlineYn: true,
-    address: null,
-    detailAddress: '강남',
-  };
-
   it('createTest', async () => {
-    const result = await activityComponent.create(activity1);
-    expect(result).toStrictEqual(Activity.createForTest(101, activity1));
-    expect(daoAccessLog).toEqual([MockActivityDao.createLog]);
-    expect(MockActivityDao.activitys.length).toBe(4);
+    const createActivityDto: ActivityCreateVO = {
+      meetingId: 'C9',
+      name: 'moeasy4',
+      explanation: '모임설명4',
+      startDate: new Date(),
+      endDate: new Date(),
+      reminder: [],
+      announcement: '공지사항4',
+      onlineYn: true,
+      address: null,
+      detailAddress: '서울',
+    };
+    const result = await activityComponent.create(createActivityDto);
+
+    expect(result.meeting_id).toBe(201);
+    expect(result.name).toBe('moeasy4');
+    expect(result.explanation).toBe('모임설명4');
+    expect(result.announcement).toBe('공지사항4');
+    expect(result.onlineYn).toBe(true);
+    expect(result.address).toBe(null);
+    expect(result.detailAddress).toBe('서울');
   });
 
   it('findByActivityIdTest', async () => {
-    const result = await activityComponent.findByActivityId(100);
-    expect(result).toStrictEqual(MockActivityDao.activitys[0]);
-    expect(daoAccessLog).toEqual([MockActivityDao.findByActivityIdLog]);
+    const activityId = 100;
+    const result = await activityComponent.findByActivityId(activityId);
+
+    expect(result.activity_id).toBe(100);
+    expect(result.name).toBe('moeasy1');
+    expect(result.explanation).toBe('모임설명1');
+    expect(result.announcement).toBe('공지사항1');
+    expect(result.onlineYn).toBe(true);
+    expect(result.address).toBe(null);
+    expect(result.detailAddress).toBe('평택');
   });
 
   it('updateTest', async () => {
-    const result = await activityComponent.update(Activity.createForTest(101, activity1));
-    expect(result).toBe(void 0);
-    expect(daoAccessLog).toEqual([MockActivityDao.updateLog]);
+    const activityId = 100;
+    const activity = Activity.createForTest(activityId, {
+      meetingId: '100',
+      name: 'moeasy1 변경',
+      explanation: '모임설명1 변경',
+      startDate: new Date(),
+      endDate: new Date(),
+      reminder: [],
+      announcement: '공지사항1 변경',
+      onlineYn: false,
+      address: null,
+      detailAddress: '평택에서 변경',
+    });
+    await activityComponent.update(activity);
+    const updatedActivity = await activityComponent.findByActivityId(100);
+
+    expect(updatedActivity.activity_id).toBe(100);
+    expect(updatedActivity.name).toBe('moeasy1 변경');
+    expect(updatedActivity.explanation).toBe('모임설명1 변경');
+    expect(updatedActivity.announcement).toBe('공지사항1 변경');
+    expect(updatedActivity.onlineYn).toBe(false);
+    expect(updatedActivity.address).toBe(null);
+    expect(updatedActivity.detailAddress).toBe('평택에서 변경');
   });
 
   it('findAllByActivityIdsTest', async () => {
-    const activityIds: number[] = [100, 300];
-    const result = await activityComponent.findAllByActivityIds(activityIds);
-    expect(result).toStrictEqual([MockActivityDao.activitys[0], MockActivityDao.activitys[2]]);
-    expect(daoAccessLog).toEqual([MockActivityDao.findAllByActivityIdsLog]);
-    expect(result.length).toBe(2);
+    const activityIds: number[] = [100, 200];
+    const results = await activityComponent.findAllByActivityIds(activityIds);
+
+    expect(results[0].activity_id).toBe(100);
+    expect(results[0].meeting_id).toBe(100);
+    expect(results[0].name).toBe('moeasy1');
+    expect(results[0].explanation).toBe('모임설명1');
+    expect(results[0].announcement).toBe('공지사항1');
+    expect(results[0].onlineYn).toBe(true);
+    expect(results[0].address).toBe(null);
+    expect(results[0].detailAddress).toBe('평택');
+
+    expect(results[1].activity_id).toBe(200);
+    expect(results[1].meeting_id).toBe(200);
+    expect(results[1].name).toBe('moeasy2');
+    expect(results[1].explanation).toBe('모임설명2');
+    expect(results[1].announcement).toBe('공지사항2');
+    expect(results[1].onlineYn).toBe(false);
+    expect(results[1].address).toBe(null);
+    expect(results[1].detailAddress).toBe('수원');
   });
 
   it('findByMeetingIdTest', async () => {
-    const result = await activityComponent.findByMeetingId(564);
-    expect(result).toStrictEqual([MockActivityDao.activitys[1], MockActivityDao.activitys[2]]);
-    expect(result.length).toBe(2);
-    expect(daoAccessLog).toEqual([MockActivityDao.findByMeetingIdLog]);
+    const meetingId = 200;
+    const results = await activityComponent.findByMeetingId(meetingId);
+
+    expect(results[0].activity_id).toBe(200);
+    expect(results[0].name).toBe('moeasy2');
+    expect(results[0].explanation).toBe('모임설명2');
+    expect(results[0].announcement).toBe('공지사항2');
+    expect(results[0].onlineYn).toBe(false);
+    expect(results[0].address).toBe(null);
+    expect(results[0].detailAddress).toBe('수원');
   });
 
   it('deleteTest', async () => {
-    const result = await activityComponent.delete(300);
-    expect(result).toBe(void 0);
-    expect(daoAccessLog).toEqual([MockActivityDao.deleteLog]);
-    expect(MockActivityDao.activitys.length).toBe(4);
+    const activityId = 200;
+
+    const beforeDelete = await activityComponent.findByActivityId(activityId);
+
+    expect(beforeDelete).toBeDefined();
+    expect(beforeDelete.activity_id).toBe(activityId);
+
+    await activityComponent.delete(activityId);
+
+    const afterDelete = await activityComponent.findByActivityId(activityId);
+    expect(afterDelete).toBeNull();
   });
 });
--- src/domain/activity/component/participant.component.spec.ts
+++ src/domain/activity/component/participant.component.spec.ts
@@ -4,58 +4,47 @@ import { Participant } from '@domain/activity/entity/participant.entity';
 import { ParticipantComponentImpl } from '@domain/activity/component/participant.component';
 import { ParticipantComponent } from '@domain/activity/component/participant.component.interface';
 
-const daoAccessLog: string[] = [];
-
 class MockParticipantDao implements ParticipantDao {
-  public static saveAllLog: string = 'NotificationDao.saveAll called';
-  public static findByUserIdAndActivityIdLog: string = 'NotificationDao.findByUserIdAndActivityId called';
-  public static findByActivityIdLog: string = 'NotificationDao.findByActivityId called';
-  public static findAllByUserIdLog: string = 'NotificationDao.findAllByUserId called';
-  public static deleteLog: string = 'NotificationDao.delete called';
-  public static deleteAllLog: string = 'NotificationDao.deleteAll called';
-
-  public static participants: Participant[] = [
-    Participant.create({ users_id: 10, activity_id: 200 }),
-    Participant.create({ users_id: 30, activity_id: 300 }),
+  public mockParticipants: Participant[] = [
+    Participant.create({ users_id: 10, activity_id: 100 }),
+    Participant.create({ users_id: 10, activity_id: 400 }),
+    Participant.create({ users_id: 30, activity_id: 200 }),
     Participant.create({ users_id: 20, activity_id: 300 }),
+    Participant.create({ users_id: 50, activity_id: 300 }),
   ];
-
-  public static getLength(): number {
-    return this.participants.length;
-  }
 
   async saveAll(participants: Participant[]): Promise<void> {
     participants.forEach((participant: Participant) => {
-      MockParticipantDao.participants.push(participant);
-    });
-    daoAccessLog.push(MockParticipantDao.saveAllLog);
-  }
-  async findByUserIdAndActivityId(user_id: number, activity_id: number): Promise<Participant | null> {
-    daoAccessLog.push(MockParticipantDao.findByUserIdAndActivityIdLog);
-    return MockParticipantDao.participants.find((participant: Participant) => {
-      if (participant.activity_id === activity_id && participant.users_id === user_id) return participant;
+      this.mockParticipants.push(participant);
     });
   }
 
-  async findByActivityId(activity_id: number): Promise<Participant[] | null> {
-    daoAccessLog.push(MockParticipantDao.findByActivityIdLog);
-    return MockParticipantDao.participants.filter((participant: Participant) => {
-      if (participant.activity_id === activity_id) return participant;
-    });
+  async findByUserIdAndActivityId(user_id: number, activity_id: number): Promise<Participant | null> {
+    return (
+      this.mockParticipants.find(
+        (participant: Participant) => participant.activity_id === activity_id && participant.users_id === user_id,
+      ) || null
+    );
+  }
+
+  async findByActivityId(activity_id: number): Promise<Participant[]> {
+    return this.mockParticipants.filter((participant: Participant) => participant.activity_id === activity_id) || null;
   }
 
   async findAllByUserId(user_id: number): Promise<Participant[] | null> {
-    daoAccessLog.push(MockParticipantDao.findAllByUserIdLog);
-    return MockParticipantDao.participants.filter((participant: Participant) => {
-      if (participant.users_id === user_id) return participant;
-    });
-  }
-  async delete(): Promise<void> {
-    daoAccessLog.push(MockParticipantDao.deleteLog);
+    return this.mockParticipants.filter((participant: Participant) => participant.users_id === user_id);
   }
 
-  async deleteAll(): Promise<void> {
-    daoAccessLog.push(MockParticipantDao.deleteAllLog);
+  async delete(userId: number, activityId: number): Promise<void> {
+    this.mockParticipants = this.mockParticipants.filter(
+      (participant: Participant) => participant.users_id !== userId && participant.activity_id !== activityId,
+    );
+  }
+
+  async deleteAll(userIds: number[], activity_id: number): Promise<void> {
+    this.mockParticipants = this.mockParticipants.filter(
+      (participant: Participant) => !userIds.includes(participant.users_id) && participant.activity_id !== activity_id,
+    );
   }
 }
 
@@ -63,66 +52,102 @@ describe('ParticipantComponent', () => {
   let participantComponent: ParticipantComponent;
 
   beforeEach(async () => {
-    daoAccessLog.length = 0;
     const module: TestingModule = await Test.createTestingModule({
       providers: [
         {
           provide: 'ParticipantDao',
           useClass: MockParticipantDao,
         },
-        { provide: 'ParticipantComponent', useClass: ParticipantComponentImpl },
+        {
+          provide: 'ParticipantComponent',
+          useClass: ParticipantComponentImpl,
+        },
       ],
     }).compile();
+
     participantComponent = module.get<ParticipantComponent>('ParticipantComponent');
   });
 
   it('saveAllTest', async () => {
-    const participants: Participant[] = [
-      Participant.create({ users_id: 10, activity_id: 100 }),
-      Participant.create({ users_id: 20, activity_id: 100 }),
+    const userId1 = 800;
+    const activityId1 = 800;
+    const userId2 = 900;
+    const activityId2 = 900;
+    const mockParticipants: Participant[] = [
+      Participant.create({ users_id: userId1, activity_id: activityId1 }),
+      Participant.create({ users_id: userId2, activity_id: activityId2 }),
     ];
-    const result = await participantComponent.saveAll(participants);
-    expect(result).toBe(void 0);
-    expect(daoAccessLog).toEqual([MockParticipantDao.saveAllLog]);
-    expect(MockParticipantDao.getLength()).toBe(5);
+
+    await participantComponent.saveAll(mockParticipants);
+
+    const result1 = await participantComponent.findByUserIdAndActivityId(userId1, activityId1);
+    const result2 = await participantComponent.findByUserIdAndActivityId(userId2, activityId2);
+
+    expect(result1.activity_id).toBe(userId1);
+    expect(result1.users_id).toBe(activityId1);
+    expect(result2.activity_id).toBe(userId2);
+    expect(result2.users_id).toBe(activityId2);
   });
 
   it('findByUserIdAndActivityIdTest', async () => {
-    const response: Participant = Participant.create({ users_id: 10, activity_id: 200 });
-    const result = await participantComponent.findByUserIdAndActivityId(10, 200);
-    expect(result).toStrictEqual(response);
-    expect(daoAccessLog).toEqual([MockParticipantDao.findByUserIdAndActivityIdLog]);
+    const userId = 10;
+    const activityId = 100;
+    const result = await participantComponent.findByUserIdAndActivityId(userId, activityId);
+
+    expect(result.activity_id).toBe(100);
+    expect(result.users_id).toBe(10);
   });
 
   it('findByActivityIdTest', async () => {
-    const result = await participantComponent.findByActivityId(300);
-    const response: Participant[] = [
-      Participant.create({ users_id: 30, activity_id: 300 }),
-      Participant.create({ users_id: 20, activity_id: 300 }),
-    ];
-    expect(result.length).toBe(2);
-    expect(result).toStrictEqual(response);
-    expect(daoAccessLog).toEqual([MockParticipantDao.findByActivityIdLog]);
+    const activityId = 300;
+    const result = await participantComponent.findByActivityId(activityId);
+
+    expect(result[0].activity_id).toBe(activityId);
+    expect(result[0].users_id).toBe(20);
+    expect(result[1].activity_id).toBe(activityId);
+    expect(result[1].users_id).toBe(50);
   });
 
   it('findAllByUserIdTest', async () => {
-    const result = await participantComponent.findAllByUserId(20);
-    const response = [
-      Participant.create({ users_id: 20, activity_id: 300 }),
-      Participant.create({ users_id: 20, activity_id: 100 }),
-    ];
-    expect(result).toStrictEqual(response);
-    expect(daoAccessLog).toEqual([MockParticipantDao.findAllByUserIdLog]);
+    const userId = 10;
+    const results = await participantComponent.findAllByUserId(userId);
+
+    expect(results[0].activity_id).toBe(100);
+    expect(results[0].users_id).toBe(10);
+    expect(results[1].activity_id).toBe(400);
+    expect(results[1].users_id).toBe(10);
   });
 
   it('deleteTest', async () => {
-    const result = await participantComponent.delete(10, 200);
-    expect(result).toBe(void 0);
-    expect(daoAccessLog).toEqual([MockParticipantDao.deleteLog]);
+    const activityId = 300;
+    const userId = 20;
+
+    const beforeDelete = await participantComponent.findByUserIdAndActivityId(userId, activityId);
+
+    expect(beforeDelete).toBeDefined();
+    expect(beforeDelete.activity_id).toBe(activityId);
+
+    await participantComponent.delete(userId, activityId);
+
+    const afterDelete = await participantComponent.findByUserIdAndActivityId(userId, activityId);
+    expect(afterDelete).toBeNull();
   });
 
   it('deleteAllTest', async () => {
-    expect(await participantComponent.deleteAll([20, 30], 300)).toBe(void 0);
-    expect(daoAccessLog).toEqual([MockParticipantDao.deleteAllLog]);
+    const activityId = 300;
+    const userIds = [20, 50];
+
+    const beforeDelete = await participantComponent.findByActivityId(activityId);
+
+    expect(beforeDelete).toBeDefined();
+    expect(beforeDelete[0].activity_id).toBe(activityId);
+    expect(beforeDelete[0].users_id).toBe(userIds[0]);
+    expect(beforeDelete[1].activity_id).toBe(activityId);
+    expect(beforeDelete[1].users_id).toBe(userIds[1]);
+
+    await participantComponent.deleteAll(userIds, activityId);
+
+    const afterDelete = await participantComponent.findByActivityId(activityId);
+    expect(afterDelete).toEqual([]);
   });
 });
